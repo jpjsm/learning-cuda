7ee4faedb017336e32afe3f17e9c65e1890118b2
JP Jofre <juanpablo.jofre@live.com>
2026-06-11 16:28:25 -0700

    updated cupy test

--- src/verify_setup/check_01_CuPy_GPU_array.py
+++ src/verify_setup/check_01_CuPy_GPU_array.py
@@ -4,4 +4,11 @@ x = cp.arange(10)
 y = x * 2
 
 print("CuPy works:", y)
-print("Device:", cp.cuda.Device())
+
+# Get number of CUDA devices
+num_devices = cp.cuda.runtime.getDeviceCount()
+# Display device IDs and names
+for i in range(num_devices):
+    with cp.cuda.Device(i):
+        name = cp.cuda.runtime.getDeviceProperties(i)["name"].decode("utf-8")
+        print(f"Device {i}: {name}")
